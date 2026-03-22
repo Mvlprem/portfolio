4d35d188acf94f3d16a1bdff9e001e0d6a09fcb5
Mvlprem <mvl.prem@outlook.com>
2026-03-22 07:31:11 +0530

    feat: add vercel SpeedInsights

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import localFont from "next/font/local";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 // Custom Fonts
 const drukWide = localFont({
@@ -46,6 +47,7 @@ export default function RootLayout({ children }) {
         {/* The Actual Content */}
         <main>{children}</main>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
